fa4776c9e48022be70b9f9fd4be2ecfee4625568
GlisseLisbeth <vilmango@hotmail.com>
2020-03-26 01:51:09 -0500

    updated colors

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+console.log('%c Oh, hello curious person,', 'background: lightgreen; color:blue; font-weight: bold; font-size: 12px; padding:0.5em',' I hope you enjoy your visit to the console //', ' （＾・ω・＾❁）');
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
 
